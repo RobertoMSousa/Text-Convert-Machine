8cf52ad7c00e7c69bb89ba112a58b4e0ec6628f5
Roberto Sousa <beto.sousa22@gmail.com>
2017-02-03 23:15:44 +0000

    change space to tabs

--- client/src/widgets/loading-animation/loading-animation.ts
+++ client/src/widgets/loading-animation/loading-animation.ts
@@ -10,15 +10,15 @@ class LoadingAnimationController {
 }
 
 angular.module('AppPlatform.widgets.loadingAnimation', [])
-.controller('LoadingAnimationController', ['$scope', AController(LoadingAnimationController)])
-.directive('loadingAnimation', [function() {
-	return {
-		restrict: 'E',
-		transclude: false,
-		scope: {
-		},
-		template: require('./loading-animation.pug')(),
-		controller: 'LoadingAnimationController',
-		controllerAs: 'ctrl'
-	};
-}]);
+	.controller('LoadingAnimationController', ['$scope', AController(LoadingAnimationController)])
+	.directive('loadingAnimation', [function() {
+		return {
+			restrict: 'E',
+			transclude: false,
+			scope: {
+			},
+			template: require('./loading-animation.pug')(),
+			controller: 'LoadingAnimationController',
+			controllerAs: 'ctrl'
+		};
+	}]);
