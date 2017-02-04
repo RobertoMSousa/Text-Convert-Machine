4202fa2f17236e6f837ed55eb308fd49794f5c19
Roberto Sousa <beto.sousa22@gmail.com>
2017-02-04 00:41:46 +0000

    add the quill editor container

--- client/src/views/create/create-controller.ts
+++ client/src/views/create/create-controller.ts
@@ -1,13 +1,24 @@
 
-
 interface ICreateControllerScope extends ng.IScope {
 	ctrl: AppCreateViewController;
 }
 
 class AppCreateViewController {
+
 	constructor(
 		private $scope: ICreateControllerScope,
 		private $state: angular.ui.IStateService) {
+		var quill = new Quill('#editor-container', {
+			modules: {
+				toolbar: [
+					[{ header: [1, 2, false] }],
+					['bold', 'italic', 'underline'],
+					['image', 'code-block']
+				]
+			},
+			placeholder: 'Compose an epic...',
+			theme: 'snow'  // or 'bubble'
+		});
 	}
 
 }
