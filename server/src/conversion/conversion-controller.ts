710c522ca4c7c5c203ee331cc6d2b894d7524bd4
Roberto Sousa <beto.sousa22@gmail.com>
2017-02-05 12:12:22 +0000

    on submit the rich text build the document and save on the DB

--- server/src/conversion/conversion-controller.ts
+++ server/src/conversion/conversion-controller.ts
@@ -1,9 +1,22 @@
 
 import express = require('express');
 import mongosome = require('../mongosome');
-import Sample = require('./conversion');
+import apiError = require('../api-helper/api-error');
+import Conversion = require('./conversion');
 
 
 export function conversionFunc(req: express.Request, res: express.Response): void {
-	res.sendStatus(200);
+	console.log('body-->', req.body);//roberto
+
+	var document: Conversion.IConversionSetDocument = {
+		documentId: new mongosome.ObjectID(),
+		delta: req.body
+	};
+
+	Conversion.collection.insert(document, (error, result: mongosome.IInsertResult<Conversion.IConversionSetDocument>) => {
+		if (error) {
+			return apiError.DatabaseError.sendError(res, error);
+		}
+		res.sendStatus(200);
+	});
 }
