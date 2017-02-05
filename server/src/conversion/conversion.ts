4e98c27c264e0af91ddf9b4413d4f1d725cfbc5e
Roberto Sousa <beto.sousa22@gmail.com>
2017-02-05 12:11:43 +0000

    create a basic interface used as reference to the conversion document

--- server/src/conversion/conversion.ts
+++ server/src/conversion/conversion.ts
@@ -4,7 +4,11 @@ import db = require('../main-db');
 
 
 export interface IConversionSetDocument extends mongosome.IDocument {
-	refId: mongosome.ObjectID;
+	documentId: mongosome.ObjectID;
+	delta: any;
+	// created: Date;
+	// status: string;
 }
+
 
 export var collection: mongosome.ICollection<IConversionSetDocument> = db.mongosome.registerCollection<IConversionSetDocument>('conversion');
