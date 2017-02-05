b2d4cc92860ff8badbf0507ee114d92e11b58f48
Roberto Sousa <beto.sousa22@gmail.com>
2017-02-05 11:13:14 +0000

    remove dev logs

--- server/src/conversion/conversion-controller.ts
+++ server/src/conversion/conversion-controller.ts
@@ -5,8 +5,5 @@ import Sample = require('./conversion');
 
 
 export function conversionFunc(req: express.Request, res: express.Response): void {
-	console.log('req.body-->', req.body);//roberto
-	console.log('req.params-->', req.params);//roberto
-	console.log('req.query-->', req.query);//roberto
 	res.sendStatus(200);
 }
