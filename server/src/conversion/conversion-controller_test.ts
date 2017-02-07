7a7bae0baecd781b6921cb1459f20db4ecfaa347
Roberto Sousa <beto.sousa22@gmail.com>
2017-02-07 18:08:27 +0000

    Create a test file and add the test db to the config

---
+++ server/src/conversion/conversion-controller_test.ts
@@ -0,0 +1,28 @@
+/// <reference path="../../../node_modules/@types/mocha/index.d.ts" />
+
+
+import chai = require('chai');
+import app = require('../application');
+import db = require('../main-db');
+import mongosome = require('../mongosome');
+import Conversion = require('./conversion');
+var expect = chai.expect;
+
+describe('conversion API', function() {
+
+	//before start clean up the DB
+	beforeEach(function(done) {
+		db.mongosome.dropDatabase(function(err) {
+			return done(err);
+		});
+	});
+
+	it('should do some test', function(done) {
+		return done();
+	});
+
+	it('should do another test', function(done) {
+		return done();
+	});
+
+});
