e1a3ce3393a82745ceb77b447be9f28935bbfaef
Roberto Sousa <beto.sousa22@gmail.com>
2017-02-09 22:21:28 +0000

    basic realtime tests

---
+++ server/src/realtime/realtime-server_test.ts
@@ -0,0 +1,20 @@
+import chai = require('chai');
+import request = require('supertest');
+import mocha = require('mocha');
+import db = require('../main-db');
+const expect = chai.expect;
+
+
+describe('Realtime server', function() {
+
+	//before start clean up the DB
+	beforeEach(function(done) {
+		db.mongosome.dropDatabase(function(err) {
+			return done(err);
+		});
+	});
+
+	it('runs the tests', function(done) {
+		done();
+	});
+});
