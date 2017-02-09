fa8d9696b52e59f95ce90e43281728f0db79756f
Roberto Sousa <beto.sousa22@gmail.com>
2017-02-09 17:44:16 +0000

    Crete some basic server tests

--- server/src/conversion/conversion-controller_test.ts
+++ server/src/conversion/conversion-controller_test.ts
@@ -1,12 +1,14 @@
 
 import chai = require('chai');
+import mocha = require('mocha');
 import app = require('../application');
 import db = require('../main-db');
 import mongosome = require('../mongosome');
 import Conversion = require('./conversion');
+import request = require('supertest');
 var expect = chai.expect;
 
-describe('conversion API', function() {
+describe.only('conversion API', function() {
 
 	//before start clean up the DB
 	beforeEach(function(done) {
@@ -15,12 +17,123 @@ describe('conversion API', function() {
 		});
 	});
 
-	it('should do some test', function(done) {
-		return done();
+	it('create html file conversion', function(done) {
+		const file = {
+			type: 'HTML',
+			title: 'sample',
+			delta: 'sampleDelta'
+		};
+		request(app.app)
+		.put('/api/conversion')
+		.set('Content-type', 'application/json')
+		.send(JSON.stringify(file))
+		.expect(200)
+		.end(done);
 	});
 
-	it('should do another test', function(done) {
-		return done();
+	it('create pdf file conversion', function(done) {
+		const file = {
+			type: 'PDF',
+			title: 'sample',
+			delta: 'sampleDelta'
+		};
+		request(app.app)
+		.put('/api/conversion')
+		.set('Content-type', 'application/json')
+		.send(JSON.stringify(file))
+		.expect(200)
+		.end(done);
 	});
+
+	it('create a conversion of a non valid file', function(done) {
+		const file = {
+			type: 'FILE',
+			title: 'sample',
+			delta: 'sampleDelta'
+		};
+		request(app.app)
+		.put('/api/conversion')
+		.set('Content-type', 'application/json')
+		.send(JSON.stringify(file))
+		.expect(403)
+		.end(done);
+	});
+
+	it('create a coversion without sending params', function(done) {
+		const file = {};
+		request(app.app)
+		.put('/api/conversion')
+		.set('Content-type', 'application/json')
+		.send(JSON.stringify(file))
+		.expect(400)
+		.end(done);
+	});
+
+	it.skip('create a conversion to pdf and get it back', function(done) {
+		const file = {
+			type: 'PDF',
+			title: 'sample',
+			delta: 'sampleDelta'
+		};
+		request(app.app)
+		.put('/api/conversion')
+		.set('Content-type', 'application/json')
+		.send(JSON.stringify(file))
+		.end(function(err, res) {
+			if (err) {
+				return done(err);
+			}
+			setTimeout(function() {
+				request(app.app)
+				.get('/api/conversion')
+				.end(function(err, res) {
+					if (err) {
+						done(err);
+						return;
+					}
+					console.log('res-->', res.body);//roberto
+					chai.expect(res.body.files.length).to.be.equal(1);
+					chai.expect(res.body.files[0].type).to.be.equal('PDF');
+					chai.expect(res.body.files[0].type).to.be.equal('sample');
+					done();
+					return;
+				});
+			}, 100); // 100 seconds
+		});
+	});
+
+	it.skip('create a conversion to html and get it back', function(done) {
+		const file = {
+			type: 'HTML',
+			title: 'sample',
+			delta: 'sampleDelta'
+		};
+		request(app.app)
+		.put('/api/conversion')
+		.set('Content-type', 'application/json')
+		.send(JSON.stringify(file))
+		.end(function(err, res) {
+			if (err) {
+				return done(err);
+			}
+			setTimeout(function() {
+				request(app.app)
+				.get('/api/conversion')
+				.end(function(err, res) {
+					if (err) {
+						done(err);
+						return;
+					}
+					console.log('res-->', res.body);//roberto
+					chai.expect(res.body.files.length).to.be.equal(1);
+					chai.expect(res.body.files[0].type).to.be.equal('HTML');
+					chai.expect(res.body.files[0].type).to.be.equal('sample');
+					done();
+					return;
+				});
+			}, 100); //10 seconds
+		});
+	});
+
 
 });
